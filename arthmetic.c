c14b562fe55d6f4b862bf743d57c08f8f62ac7e7
Khyati_Singh <99485337+SinghSahabbb@users.noreply.github.com>
2023-11-22 14:03:28 +0530

    commit

--- arthmetic.c
+++ arthmetic.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int first, second, add, subtract, multiply;
+    float divide;
+    
+    printf("Enter two integers\n");
+    scanf("%d%d", &first, &second);
+    add = first + second;
+    subtract = first - second;
+    multiply = first * second;
+    divide     = first / (float)second;
+    printf("hello sir");
+    printf("Sum = %d\n",add);
+    printf("Difference = %d\n",subtract);
+    printf("Multiplication of numbers = %d\n",multiply);
+    printf("Division of numbers = %.2f\n",divide);
+    
+    return 0;
+}
 
--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main() {
    printf("Hello, World!");
+   printf("hi");
+   printf("namaste");
+   printf("sat siya kaal");
+   printf("wwhat");
    return 0;
 }
