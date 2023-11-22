ed2a84d3a5e3849d3024ac7b917a5db012a1fb01
Khyati_Singh <99485337+SinghSahabbb@users.noreply.github.com>
2023-11-22 14:03:28 +0530

    Update arthmetic.c

--- arthmetic.c
+++ arthmetic.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int first, second, add, subtract, multiply;
+    float divide;
+    
+    printf("Enter two integers\n");
+    scanf("%d%d", &first, &second);
+    add = first + second;
+    subtract = first - second;
+    printf("Sum = %d\n",add);
+    printf("Difference = %d\n",subtract);
+    
+    return 0;
+}
 
